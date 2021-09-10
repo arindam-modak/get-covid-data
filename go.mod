eb1e32d8cb285744eddd81d6954259cb9955e62c
arindam-modak <arindam.modak03@gmail.com>
2021-09-10 15:24:49 +0530

    Fetching covid data and save to mongoDB part done, refactoring needed

--- go.mod
+++ go.mod
@@ -3,14 +3,25 @@ module github.com/arindam-modak/get-covid-data
 go 1.17
 
 require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/gocarina/gocsv v0.0.0-20210516172204-ca9e8a8ddea8 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.9.5 // indirect
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.2 // indirect
 	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.0.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	go.mongodb.org/mongo-driver v1.7.2 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 	golang.org/x/net v0.0.0-20210226172049-e18ecbb05110 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
-	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/text v0.3.5 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
+	"github.com/gocarina/gocsv"
 	"github.com/labstack/echo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+// State,Confirmed,Recovered,Deaths,Active,Last_Updated_Time,Migrated_Other,State_code,Delta_Confirmed,Delta_Recovered,Delta_Deaths,State_Notes
+
+type CovidData struct { // Our example struct, you can use "-" to ignore a field
+	State           string `csv:"State"`
+	Confirmed       int    `csv:"Confirmed"`
+	Recovered       int    `csv:"Recovered"`
+	LastUpdatedTime string `csv:"Last_Updated_Time"`
+}
+
+/*
+   Define my document struct
+*/
+type CovidDataDB struct {
+	State           string `bson:"state,omitempty"`
+	ConfirmedCases  int    `bson:"confirmed_cases,omitempty"`
+	RecoveredCases  int    `bson:"recovered_cases,omitempty"`
+	LastUpdatedTime string `bson:"last_updated_time,omitempty"`
+	DataUpdtedAt    string `bson:"data_updated_at,omitempty"`
+}
 
 func main() {
 	fmt.Println("Hello Covid! Please go away now.")
@@ -13,6 +42,111 @@ func main() {
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello from web server")
+	})
+
+	e.GET("/fetch-data-and-save", func(c echo.Context) error {
+		response, err := http.Get(os.Getenv("COVID_DATA_URL"))
+		if err != nil {
+			fmt.Print(err.Error())
+			os.Exit(1)
+		}
+
+		responseData, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(string(responseData))
+
+		f, err := os.Create("data.csv")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer f.Close()
+
+		_, err2 := f.Write(responseData)
+
+		if err2 != nil {
+			log.Fatal(err2)
+		}
+
+		covidDataFile, err := os.OpenFile("data.csv", os.O_RDWR|os.O_CREATE, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+		defer covidDataFile.Close()
+
+		covidData := []*CovidData{}
+
+		if err := gocsv.UnmarshalFile(covidDataFile, &covidData); err != nil { // Load clients from file
+			panic(err)
+		}
+
+		var docs []interface{}
+
+		for _, client := range covidData {
+			fmt.Println("Number of Cases ", client.State, client.Confirmed, client.LastUpdatedTime)
+			docs = append(docs, bson.D{{"state", client.State}, {"confirmed_cases", client.Confirmed}, {"recovered_cases", client.Recovered}, {"last_updated_time", client.LastUpdatedTime}, {"data_updated_at", time.Now().String()}})
+		}
+
+		/*
+		   Connect to my cluster
+		*/
+		dbUri := fmt.Sprintf("mongodb+srv://%s:%s@%s?retryWrites=true&w=majority", os.Getenv("MONGO_USERNAME"), os.Getenv("MONGO_PASSWORD"), os.Getenv("MONGO_HOST"))
+		client, err := mongo.NewClient(options.Client().ApplyURI(dbUri))
+		if err != nil {
+			log.Fatal(err)
+		}
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		err = client.Connect(ctx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer client.Disconnect(ctx)
+
+		/*
+			List databases
+		*/
+		databases, err := client.ListDatabaseNames(ctx, bson.M{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(databases)
+
+		/*
+		   Get my collection instance
+		*/
+
+		collection := client.Database(os.Getenv("MONGO_DB_NAME")).Collection(os.Getenv("MONGO_COLLECTION_NAME"))
+		collection.Drop(ctx)
+
+		/*
+			Insert documents
+		*/
+
+		res, insertErr := collection.InsertMany(ctx, docs)
+		if insertErr != nil {
+			log.Fatal(insertErr)
+		}
+		fmt.Println(res)
+		/*
+			Iterate a cursor and print it
+		*/
+		cur, currErr := collection.Find(ctx, bson.D{})
+
+		if currErr != nil {
+			panic(currErr)
+		}
+		defer cur.Close(ctx)
+
+		var covidDataDB []CovidDataDB
+		if err = cur.All(ctx, &covidDataDB); err != nil {
+			panic(err)
+		}
+		fmt.Println(covidDataDB)
+
+		return c.String(http.StatusOK, "Latest Data Fetched")
 	})
 
 	e.Start(":8000")
